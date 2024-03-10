22739bc2ba537f21d3e0bf75e0f72dd8fe28971a
Natalie Gillam <ngillam@hotmail.co.uk>
2024-03-10 19:50:22 +0000

    initial commit

---
+++ js/questions.js
@@ -0,0 +1,124 @@
+const questions = [
+  {
+    id: 1,
+    q: "What happens in the <em class='underline'>fetch</em> stage of the fetch-execute-decode (FDE) cycle?",
+    choices: [
+      "An instruction is moved from memory to the CPU",
+      "The instruction is interpreted by the CPU",
+      "The instruction is carried out",
+      "The instruction is carried out, then the FDE process is repeated",
+    ],
+    answer: 0,
+    topic: "CPU",
+  },
+  {
+    id: 2,
+    q: "What happens in the <em class='underline'>decode</em> stage of the fetch-execute-decode (FDE) cycle?",
+    choices: [
+      "The instruction is interpreted by the CPU",
+      "The instruction is carried out",
+      "An instruction is moved from memory to the CPU",
+      "The instruction is carried out, then the FDE process is repeated",
+    ],
+    answer: 0,
+    topic: "CPU",
+  },
+  {
+    id: 3,
+    q: "What happens in the <em class='underline'>execute</em> stage of the fetch-execute-decode (FDE) cycle?",
+    choices: [
+      "The instruction is carried out, then the FDE process is repeated",
+"The instruction is carried out",
+      "An instruction is moved from memory to the CPU",
+      "The instruction is interpreted by the CPU",
+    ],
+    answer: 0,
+    topic: "CPU",
+  },
+  {
+    id: 4,
+    q: "Which of these describes the <em class='underline'>Control Unit (CU)</em>?",
+    choices: [
+      "interprets instructions, directs data flow within the CPU and memory, and coordinates execution of operations.",
+      "performs arithmetic and logical operations such as addition, subtraction, multiplication, division, AND, OR, and NOT",
+      "discrete storage locations in the CPU used to temporarily hold data, instructions, or addresses during the FDE cycle.",
+      "small, high-speed memory in the CPU that stores frequently and recently used data."
+    ],
+    answer: 0,
+    topic: "CPU",
+  },
+  {
+    id: 5,
+    q: "Which of these describes the <em class='underline'>Arithmetic Logic Unit (ALU)</em>?",
+    choices: [
+      "performs arithmetic and logical operations such as addition, subtraction, multiplication, division, AND, OR, and NOT",
+      "discrete storage locations in the CPU used to temporarily hold data, instructions, or addresses during the FDE cycle.",
+      "small, high-speed memory in the CPU that stores frequently and recently used data.",
+      "interprets instructions, directs data flow within the CPU and memory, and coordinates execution of operations.",
+    ],
+    answer: 0,
+    topic: "CPU",
+  },
+  {
+    id: 6,
+    q: "Which of these describes a CPU <em class='underline'>register</em>?",
+    choices: [
+      "discrete storage locations in the CPU used to temporarily hold data, instructions, or addresses during the FDE cycle.",
+      "small, high-speed memory in the CPU that stores frequently and recently used data.",
+      "interprets instructions, directs data flow within the CPU and memory, and coordinates execution of operations.",
+      "performs arithmetic and logical operations such as addition, subtraction, multiplication, division, AND, OR, and NOT",
+    ],
+    answer: 0,
+    topic: "CPU",
+  },
+  {
+    id: 7,
+    q: "Which of these describes <em class='underline'>cache memory</em>?",
+    choices: [
+      "small, high-speed memory in the CPU that stores frequently and recently used data.",
+      "interprets instructions, directs data flow within the CPU and memory, and coordinates execution of operations.",
+      "performs arithmetic and logical operations such as addition, subtraction, multiplication, division, AND, OR, and NOT",
+      "discrete storage locations in the CPU used to temporarily hold data, instructions, or addresses during the FDE cycle.",
+      
+    ],
+    answer: 0,
+    topic: "CPU",
+  },
+  {
+    id: 8,
+    q: "What is the key principle of Von Neumann architecture?",
+    choices: [
+      "computer program instructions and data are stored in the <em class='bold'>same memory</em>",
+      "computer program instructions and data are stored in <em class='bold'>different memory</em>",
+    ],
+    answer: 0,
+    topic: "CPU",
+  },
+  {
+    id: 9,
+    q: "Which statement is correct regarding Von Neumann architecture?",
+    choices: [
+      "Von Neumann architecture makes it easier to change a computer program",
+      "Von Neumann architecture makes it harder to change a computer program",
+    ],
+    answer: 0,
+    topic: "CPU",
+  },
+  {
+    id: 10,
+    q: "Which hardware is required for a CPU that uses Von Neumann architecture?",
+    choices: [
+      "Control Unit, Arithmetic Logic Unit, buses, registers, input devices and output devices",
+      "Control Unit, Arithmetic Logic Unit, buses, registers, retinal scanner and output devices",
+      "Control Unit, Arithmetic Logic Unit, cables, registers, input devices and output devices",
+      "Control Unit, Arithmetic Logic Unit, buses, registers, input devices, output devices and uninterruptible power supply",
+    ],
+    answer: 0,
+    topic: "CPU",
+  },
+
+
+
+
+
+]
